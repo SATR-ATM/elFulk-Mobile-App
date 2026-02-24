9c2a43f05e0ab7c628394da1de38139402a668af
Abdelghani Djedidi <abdelghani.djedidi.2022@gmail.com>
2026-02-24 01:11:57 +0100

    🏗️ build(firebase): use example production options import

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,7 +1,7 @@
+import 'package:elfulk/firebase_options_production.example.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'firebase_options_production.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
